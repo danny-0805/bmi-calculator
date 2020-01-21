1447b113b6ec619d10413eb4d3346c668cc706f1
Dan <dan.devguru@gmail.com>
2020-01-21 12:28:53 -0600

    Set background for input box

--- src/components/elements/pInput.js
+++ src/components/elements/pInput.js
@@ -4,9 +4,9 @@ import PropTypes from "prop-types"
 
 const PInput = (props) => (
   <div className="flex flex-wrap px-4 py-3 border-b border-gray-400">
-    <div className="w-6/12 text-left">{props.caption}</div>
-    <div className="w-6/12 text-right flex flex-wrap justify-end">
-      <input className="w-10 text-gray-500" type="text" placeholder="-" value={props.amount} onChange={props.handleChange}/>
+    <div className="w-4/12 sm:w-6/12 text-left">{props.caption}</div>
+    <div className="w-8/12 sm:w-6/12 text-right flex flex-wrap justify-end">
+      <input className="w-10 text-gray-500 bg-gray-200 rounded" type="text" placeholder="-" value={props.amount} onChange={props.handleChange}/>
       <span className="mr-2 text-gray-500 w-6 text-left">{`${props.unit}`}</span>
       <ButtonGroup
         isZero={!props.amount}
--- src/components/elements/unitSelect.js
+++ src/components/elements/unitSelect.js
@@ -5,7 +5,7 @@ const UnitSelect = (props) => (
   <div className="flex flex-wrap px-4 py-3 border-b border-gray-400">
     <div className="w-6/12 text-left">{props.caption}</div>
     <div className="w-6/12 text-right flex justify-end">
-      <select className="block w-32" value={props.unit} onBlur={props.changeUnit}>
+      <select className="block w-32" value={props.unit} onBlur={props.changeUnit} onChange={props.changeUnit}>
         <option value="us">US customary</option>
         {/* <option value="si">International System</option> */}
       </select>
